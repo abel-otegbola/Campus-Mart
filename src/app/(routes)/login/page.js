9606b288b6b699a1836472a07c1a320c3fe4eafd
abel-otegbola <abel15655@gmail.com>
2023-12-06 05:43:34 +0100

    feat: add inputField and button to login page

--- src/app/(routes)/login/page.js
+++ src/app/(routes)/login/page.js
@@ -1,12 +1,29 @@
+'use client'
+import InputField from "@/app/components/inputField/page"
 import Button from "../../components/button/page"
+import { useState } from "react"
+import { PiStorefrontLight, PiUserCircleLight } from "react-icons/pi"
 
 export default function Login() {
+  const [account, setAccount] = useState("Buyer's account")
+  const [fullname, setFullname] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <main className="">
-      <header className="flex flex-col md:px-[30%] p-[5%] min-h-[80vh] md:items-center items-start justify-center sm:text-center">
-        <h1 className="md:text-[44px] text-[30px] font-[700] text-[#5938DD]">Welcome</h1>
-        <p className="py-4">Create account now</p>
-        <Button text={"Continue"} to={"#"} />
+      <header className="flex flex-col p-[5%] min-h-[80vh] mx-auto sm:w-[470px] w-full items-center justify-center">
+        <h1 className="md:text-[38px] text-[30px] font-[700] text-[#5938DD]">Welcome</h1>
+        <p className="py-4">Login to continue</p>
+
+        <InputField label={"Email Address"} action={setEmail} type={"email"} />
+        <InputField label={"Password"} action={setPassword} type={"password"} />
+
+        <a href="/forgot-password" className="py-2 text-[#5938DD]">Forgot password?</a>
+
+        <Button text={"Log in"} to={"#"} type={"long"} />
+
+        <p>Don't have an account? <a href="/signup" className="py-2 text-[#5938DD]">Signup</a></p>
 
       </header>
     </main>
