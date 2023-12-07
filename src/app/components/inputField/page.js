8be2a60023cede3d8d0461e801c6dd2074f477a1
abel-otegbola <abel15655@gmail.com>
2023-12-07 13:40:24 +0100

    fix: border color for navbar mobile

--- src/app/components/inputField/page.js
+++ src/app/components/inputField/page.js
@@ -12,7 +12,7 @@ export default function InputField({ type, label, action,  }) {
         <div className="relative py-4 w-full">
             <label className={`absolute dark:bg-black bg-white p-[2px] px-2 transition-all ${focused ? "text-blue -top-2 left-0 text-[11px]" : 'left-2 top-[5px] text-[10px]'}`} htmlFor={label}>{label}</label>
             <input 
-                className="w-full bg-white dark:bg-black border border-gray-400 dark:border-gray-800 rounded p-[10px] bg-white focus:outline outline-blue/[0.3] hover:border-blue outline-offset-1" 
+                className="w-full bg-white dark:bg-black border border-gray-400 dark:border-gray-600 rounded p-[10px] bg-white focus:outline outline-blue/[0.3] hover:border-blue outline-offset-1" 
                 id={label} 
                 type={type} 
                 name={label} 
--- src/app/components/navbar/page.js
+++ src/app/components/navbar/page.js
@@ -11,11 +11,11 @@ export default function Navbar() {
         <div className="flex sticky top-0 left-0 bg-white dark:bg-black justify-between items-center p-2 md:px-[8%] px-[3%] border border-transparent border-y-gray-100 dark:border-y-gray-900 z-[1]">
             <a href='/' className="sm:w-[20%] text-blue font-bold text-[18px]">Campus Mart</a>
 
-            <nav className="flex sm:static dark:bg-black bg-white fixed bottom-0 left-0 md:w-auto gap-2 w-full justify-between sm:border-none border border-y-gray-100 z-[2]">
-                <a href="/" className={`flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/" ? activeClass : ""}`}><PiBuildingsLight/> <span className="sm:text-[14px] text-[9px]">Home</span></a>
-                <a href="/stores" className={`flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/stores" ? activeClass : ""}`}><PiStorefrontLight /> <span className="sm:text-[14px] text-[9px]">Stores</span></a>
-                <a href="/wishlist" className={`flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/wishlist" ? activeClass : ""}`}><PiShoppingBagLight /> <span className="sm:text-[14px] text-[9px]">Wishlist</span></a>
-                <a href="/dashboard" className={`sm:hidden flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/dashboard" ? activeClass : ""}`}><PiUserCircleLight /> <span className="sm:text-[14px] text-[9px]">Dashboard</span></a>
+            <nav className="flex sm:static dark:bg-black bg-white fixed bottom-0 left-0 md:w-auto gap-2 w-full justify-between sm:border-none border transparent border-y-gray-100 dark:border-y-gray-900 z-[2]">
+                <a href="/" className={`flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 sm:hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/" ? activeClass : ""}`}><PiBuildingsLight/> <span className="sm:text-[14px] text-[9px]">Home</span></a>
+                <a href="/stores" className={`flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 sm:hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/stores" ? activeClass : ""}`}><PiStorefrontLight /> <span className="sm:text-[14px] text-[9px]">Stores</span></a>
+                <a href="/wishlist" className={`flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 sm:hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/wishlist" ? activeClass : ""}`}><PiShoppingBagLight /> <span className="sm:text-[14px] text-[9px]">Wishlist</span></a>
+                <a href="/dashboard" className={`sm:hidden flex sm:flex-row flex-col items-center gap-2 p-[12px] px-6 sm:hover:bg-blue/[0.07] hover:text-blue text-[20px] rounded-full ${pathname === "/dashboard" ? activeClass : ""}`}><PiUserCircleLight /> <span className="sm:text-[14px] text-[9px]">Dashboard</span></a>
             </nav>
 
             <div className="flex justify-end gap-6 sm:w-[20%]">
