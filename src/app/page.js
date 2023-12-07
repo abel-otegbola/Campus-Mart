f542fe020b0c6e4559a72b5011666b831a47b52d
abel-otegbola <abel15655@gmail.com>
2023-12-07 15:02:08 +0100

    fix: eslint error for quot removed

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="">
       <header className="flex flex-col md:px-[30%] p-[3%] min-h-[80vh] md:items-center items-start justify-center md:text-center">
         <h1 className="md:text-[44px] text-[30px] font-[700] text-blue">Campus Mart</h1>
-        <p className="py-4">"Discover, Trade, Thrive: Your Campus, Your Marketplace!"</p>
+        <p className="py-4">Discover, Trade, Thrive: Your Campus, Your Marketplace!</p>
         <Button text={"Join the waitlist"} to={"/login"} />
 
       </header>
