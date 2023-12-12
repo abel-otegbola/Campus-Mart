7f3f2fbe02c8316b893f9953c5391916c2da157c
abel-otegbola <abel15655@gmail.com>
2023-12-12 19:58:08 +0100

    feat: add background hero image

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 import Button from "./components/button/page"
 import { data } from "./data/projects"
 import ProductCard from "./components/productCard/page"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="">
 
-      <header className="flex flex-col md:px-[30%] p-[3%] min-h-[80vh] md:items-center items-start justify-center md:text-center">
+      <header className="flex flex-col md:px-[30%] p-[3%] md:py-[3%] py-[20%] md:items-center items-start justify-center md:text-center">
         <h1 className="md:text-[44px] text-[30px] font-[700] text-blue">Campus Mart</h1>
         <p className="py-4">Discover, Trade, Thrive: Your Campus, Your Marketplace!</p>
         <Button text={"Join the waitlist"} to={"/login"} />
+        <Image src="/background.png" className="my-6" width={500} height={400} alt="background" />
       </header>
 
       <section className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 sm:gap-4 gap-2 md:px-[8%] sm:p-[3%] p-1">
