c41f94d719f3718154eec3b18b65c63e2c3cc027
abel-otegbola <abel15655@gmail.com>
2023-12-15 13:18:57 +0100

    feat: add menu popup to navbar

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,15 @@ export default function Home() {
   return (
     <main className="">
 
-      <header className="flex flex-col md:px-[30%] p-[3%] md:py-[3%] py-[20%] md:items-center items-start justify-center md:text-center">
-        <h1 className="md:text-[44px] text-[30px] font-[700] text-blue">Campus Mart</h1>
-        <p className="py-4">Discover, Trade, Thrive: Your Campus, Your Marketplace!</p>
-        <Button text={"Join the waitlist"} to={"/login"} />
-        <Image src="/background.png" className="my-6" width={500} height={400} alt="background" />
+      <header className="md:flex md:px-[8%] p-[3%] md:py-[3%] py-[20%] md:items-center items-start justify-between">
+        <div className="md:w-auto w-full">
+          <h1 className="md:text-[44px] text-[30px] font-[700] text-blue">Campus Mart</h1>
+          <p className="py-4">Discover, Trade, Thrive: Your Campus, Your Marketplace!</p>
+          <Button text={"Join the waitlist"} to={"/login"} />
+        </div>
+        <div className="md:w-auto w-full flex flex-1 justify-center items-center">
+          <Image src="/background.png" className="my-6" width={500} height={400} alt="background" />
+        </div>
       </header>
 
       <section className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 sm:gap-4 gap-2 md:px-[8%] sm:p-[3%] p-1">
