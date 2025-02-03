a39e3d789d26cbc2e2a4857a176796e2ba4e55d2
ABEL OTEGBOLA <abel15655@gmail.com>
2025-02-03 11:34:33 +0100

    fix: totla price component error

--- src/components/totalPrice/totalPrice.tsx
+++ src/components/totalPrice/totalPrice.tsx
@@ -11,8 +11,8 @@ export default function TotalPrice ({ discount, order }: { discount?: number, or
         <p>
         {
             currencyFormatter(
-                products.filter((item: IProduct) => (order ? order.cart : cart).map((item: ICart) => item.id).indexOf(item.id) !== -1 )
-                .map((product: IProduct) => {return {price: +product?.price * cart.filter((item: ICart) => item.id === product?.id)[0]?.quantity}})
+                products.filter((item: IProduct) => (order ? order.cart : cart).map((item: ICart) => item.id).indexOf(item._id) !== -1 )
+                .map((product: IProduct) => {return {price: +product?.price * cart.filter((item: ICart) => item.id === product?._id)[0]?.quantity}})
                 .reduce((a: number,v: { price: number }) => a = a + v.price, 0) - (discount || 0)
             )
         }
